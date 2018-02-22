c8313780455a62102852d28bf586cfdcc703a7a8
Mingfei <Mingfei.Yang.1@asu.edu>
2018-02-21 19:52:20 -0700

    617. Merge Two Binary Trees
    Updated version, removed unnecessary recursions to improve performance.

--- Java/MergeBinaryTrees.java
+++ Java/MergeBinaryTrees.java
@@ -1,4 +1,5 @@
 // 617. Merge Two Binary Trees
+// Recursive way
 
 /**
  * Definition for a binary tree node.
@@ -9,34 +10,35 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- 
-public class Solution {
+
+class Solution {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        TreeNode nNode;
         
-        if (t1 != null && t2 != null) {
-            // Both trees are not null
-            nNode = new TreeNode(t1.val + t2.val);
-            nNode.left = mergeTrees(t1.left, t2.left);
-            nNode.right = mergeTrees(t1.right, t2.right);
-        }
-        else if (t1 != null && t2 == null) {
-            // t2 is null
-            nNode = new TreeNode(t1.val);
-            nNode.left = mergeTrees(t1.left, null);
-            nNode.right = mergeTrees(t1.right, null);
-        }
-        else if (t1 == null && t2 != null) {
-            // t1 is null
-            nNode = new TreeNode(t2.val);
-            nNode.left = mergeTrees(null, t2.left);
-            nNode.right = mergeTrees(null, t2.right);
-        }
-        else {
-            // Both are null, return null
+        // If t1 and t2 are NULL
+        if (t1 == null && t2 == null) {
             return null;
         }
-
-        return nNode;
+        // t2 == null, return t1
+        else if (t1 != null && t2 == null) {
+            return t1;
+        }
+        // t1 == null, return t2
+        else if (t1 == null && t2 != null) {
+            return t2;
+        }
+        // Both are NOT NULL
+        // Merge trees Recursively
+        else {
+            int val_t1 = t1 == null ? 0 : t1.val;
+            int val_t2 = t2 == null ? 0 : t2.val;
+        
+            TreeNode root = new TreeNode(val_t1 + val_t2);
+            root.left = mergeTrees(t1.left, t2.left);
+            root.right = mergeTrees(t1.right, t2.right);
+        
+            return root;
+        }
+        
+        
     }
 }
